dcf62a990fb944f4bb99e2e1b8fcc1bba7ca7185
Olha Morozova <olhamorozowa@gmail.com>
2022-02-23 16:51:30 +0200

    slide

---
+++ index.js
@@ -0,0 +1,58 @@
+let currentSlide = 0;
+
+document.body.innerHTML = buildSlider(3);
+
+const slider = document.querySelector('.slider')
+const track = slider.querySelector('.slide-track')
+
+const [leftBtn, rightBtn] = slider.querySelectorAll('button')
+
+leftBtn.onclick = slideLeft
+rightBtn.onclick = slideRight
+
+let timer = setInterval(slideRight, 2000)
+slider.onmouseover = ()=> clearInterval(timer)
+slider.onmouseout = ()=> timer = setInterval(slideRight, 2000)
+
+function buildSlide() {
+    return `
+        <li class="slide">
+            <h3>Header</h3>
+            <div class="img"></div>
+        </li>
+    `
+}
+
+function buildSlider(length) {
+    return `
+        <div class="slider">
+            <button></button>
+            <div class="slide-holder">
+                <ul class="slide-track">
+                    ${buildSlide().repeat(length)}
+                </ul>
+            </div>
+            <button></button>
+        </div>
+    `
+}
+
+function slideLeft() {
+    if (currentSlide === 0) {
+        track.prepend(track.lastElementChild)
+        currentSlide++ ;
+        track.children[currentSlide].scrollIntoView()
+    }
+    currentSlide--
+    track.children[currentSlide].scrollIntoView({behavior: 'smooth'})
+}
+
+function slideRight() {
+    if (currentSlide === track.children.length - 1) {
+        track.append(track.firstElementChild)
+        currentSlide-- ;
+        track.children[currentSlide].scrollIntoView()
+    }
+    currentSlide++
+    track.children[currentSlide].scrollIntoView({behavior: 'smooth'})
+}
